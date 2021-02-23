10bd801696f6609041ac7f2c290da1a9790f396e
Lucas <lucas.cabral@virtus.ufcg.edu.br>
2021-02-23 12:36:52 -0300

    Menu component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import Menu from './components/MenuComponent';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar dart color="primary">
+    <div>
+      <Navbar dart color="dark">
         <div className="container">
            <NavbarBrand href="/"> Ristorante Con Fusion </NavbarBrand>
         </div>
       </Navbar>
+
+        <Menu />
     </div>
   );
 }
