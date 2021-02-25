f7b433d0e558e0884f39b045d18da0e9738a1d38
Lucas <lucas.cabral@virtus.ufcg.edu.br>
2021-02-25 18:44:46 -0300

    Adding Fetch from server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -6,6 +6,7 @@ import { Breadcrumb, BreadcrumbItem, Button,
 import { Link } from 'react-router-dom';
 import { Control, Errors, LocalForm } from "react-redux-form";
 import { Loading } from './LoadingComponent';
+import { baseUrl } from "../shared/baseUrl";
 
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -108,7 +109,7 @@ function RenderDishDetails({dish}) {
     return(
         <div className="col-12 col-md-5 m-1">
             <Card>
-                <CardImg top src={dish.image} alt={dish.name} />
+                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                 <CardBody>
                     <CardTitle>{dish.name}</CardTitle>
                     <CardText>{dish.description}</CardText>
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody,
     CardTitle, CardSubtitle } from 'reactstrap';
-import {Loading} from "./LoadingComponent";
+import { Loading } from "./LoadingComponent";
+import { baseUrl } from "../shared/baseUrl";
 
 function RenderCard({item, isLoading, errMess}){
     if (isLoading) {
@@ -15,7 +16,7 @@ function RenderCard({item, isLoading, errMess}){
     } else {
         return (
             <Card>
-                <CardImg top src={item.image} alt={item.name} />
+                <CardImg src={baseUrl + item.image} alt={item.name} />
                 <CardBody>
                     <CardTitle>{item.name}</CardTitle>
                     {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle>: null}
@@ -37,7 +38,9 @@ function Home (props) {
               </div>
 
               <div className="col-12 col-md m-1">
-                  <RenderCard item={props.promotion} />
+                  <RenderCard item={props.promotion}
+                              isLoading={props.promosLoading}
+                              errMss={props.promosErrMess} />
               </div>
 
               <div className="col-12 col-md m-1">
