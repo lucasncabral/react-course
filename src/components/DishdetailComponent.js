493484b23f228aede5b4ce897a47f1336803f455
Lucas <lucas.cabral@virtus.ufcg.edu.br>
2021-02-25 20:00:05 -0300

    React animations

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 import { Control, Errors, LocalForm } from "react-redux-form";
 import { Loading } from './LoadingComponent';
 import { baseUrl } from "../shared/baseUrl";
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -108,30 +109,42 @@ class CommentForm extends Component {
 function RenderDishDetails({dish}) {
     return(
         <div className="col-12 col-md-5 m-1">
-            <Card>
-                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
-                <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                </CardBody>
-            </Card>
+            <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+                <Card>
+                    <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </FadeTransform>
         </div>
+
     );
 }
 
 function RenderComments({comments, postComment, dishId}) {
     let dateFormat = { year: 'numeric', month: 'short', day: 'numeric' };
     const lista = comments.map((item) =>
-        <ul className="list-unstyled">
-            <li>{item.comment}</li>
-            <li>-- {item.author}, <span>{new Date(item.date).toLocaleDateString("en-US", dateFormat)}</span></li>
-        </ul>
+
+        <Fade in>
+            <ul className="list-unstyled">
+                <li>{item.comment}</li>
+                <li>-- {item.author}, <span>{new Date(item.date).toLocaleDateString("en-US", dateFormat)}</span></li>
+            </ul>
+        </Fade>
     );
     if(comments != null)
         return(
             <div className="col-12 col-md-5 m-1">
                 <h4>Comments</h4>
-                {lista}
+                <Stagger in>
+                    {lista}
+                </Stagger>
                 <CommentForm dishId={dishId} postComment={postComment} />
             </div>
         );
